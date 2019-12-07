ceab6610098f9e5c09469760ffe49b4ea8e24c7b
Dlee4428 <44447609+Dlee4428@users.noreply.github.com>
2019-12-07 17:13:32 -0500

    tlc

---
+++ AI_Group/Assets/Scripts/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] public MainMenuManager manager;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision");
+            SceneManager.LoadScene(manager.GetComponent<MainMenuManager>().mainMenu);
+        }
+    }
+}
---
+++ AI_Group/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Data")]
+    [SerializeField] public string mainMenu = "MainMenu";
+    [SerializeField] public string test = "Main";
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(test);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
